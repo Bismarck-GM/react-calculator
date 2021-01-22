b84f5965fedbbf299371db7298d70f96b4a73280
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-01-22 13:32:08 -0300

    Add Components folder. Move App.js to components. Add Display.js.

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="App">
-      Hello there!
-    </div>
-  );
-}
-
-export default App;
---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import Display from './Display';
+
+function App() {
+  return (
+    <>
+      <Display result="0" />
+    </>
+  );
+}
+
+export default App;
