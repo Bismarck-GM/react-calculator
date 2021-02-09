88af85bc041d582da3a6c3bedff4ee2a0f52a066
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-02-09 13:18:56 -0300

    Edit App to be Class Component. Add HandleClick function.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,32 @@
+import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-// eslint-disable-next-line no-unused-vars
-import Calculate from '../logic/calculate';
+import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <>
-      <Display result="0" />
-      <ButtonPanel />
-    </>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const data = this.state;
+    this.setState(calculate(data, buttonName));
+  }
+
+  render() {
+    const data = this.state;
+    return (
+      <>
+        <Display result={data.next || data.total || '0'} />
+        <ButtonPanel />
+      </>
+    );
+  }
 }
-
-export default App;
