cff17b75783921c3de8662ff491adba640d98303
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-02-16 13:22:52 -0300

    Add basic style to App.js.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-width: 700px;
+  align-items: center;
+  justify-content: center; 
+`;
 
 const App = () => {
   const [data, setData] = useState({ total: null, next: null, operation: null });
@@ -10,10 +19,10 @@ const App = () => {
     setData(calculate(data, buttonName));
   };
   return (
-    <>
+    <Container>
       <Display result={data.next || data.total || '0'} />
       <ButtonPanel clickHandler={handleClick} />
-    </>
+    </Container>
   );
 };
 
