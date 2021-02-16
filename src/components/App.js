40d21f309d15e6b0ee22957c6753cf9dfb24e037
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-02-16 13:11:36 -0300

    Delete App.js commented code.

--- src/components/App.js
+++ src/components/App.js
@@ -3,35 +3,6 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-// export default class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       total: null,
-//       next: null,
-//       // eslint-disable-next-line react/no-unused-state
-//       operation: null,
-//     };
-//     this.handleClick = this.handleClick.bind(this);
-//   }
-
-//   handleClick(buttonName) {
-//     const data = this.state;
-//     this.setState(calculate(data, buttonName));
-//   }
-
-//   render() {
-//     const { next, total } = this.state;
-//     return (
-//       <>
-//         <Display result={next || total || '0'} />
-//         <ButtonPanel clickHandler={this.handleClick} />
-//       </>
-//     );
-//   }
-// }
-
 const App = () => {
   const [data, setData] = useState({ total: null, next: null, operation: null });
 
