add5a6818f1f31cd33228a5dc07e17570d8ad449
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-02-16 13:01:56 -0300

    Edit refactor code in App and Calculate.

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
+// export default class App extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      total: null,
-      next: null,
-      // eslint-disable-next-line react/no-unused-state
-      operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+//     this.state = {
+//       total: null,
+//       next: null,
+//       // eslint-disable-next-line react/no-unused-state
+//       operation: null,
+//     };
+//     this.handleClick = this.handleClick.bind(this);
+//   }
 
-  handleClick(buttonName) {
-    const data = this.state;
-    this.setState(calculate(data, buttonName));
-  }
+//   handleClick(buttonName) {
+//     const data = this.state;
+//     this.setState(calculate(data, buttonName));
+//   }
 
-  render() {
-    const { next, total } = this.state;
-    return (
-      <>
-        <Display result={next || total || '0'} />
-        <ButtonPanel clickHandler={this.handleClick} />
-      </>
-    );
-  }
-}
+//   render() {
+//     const { next, total } = this.state;
+//     return (
+//       <>
+//         <Display result={next || total || '0'} />
+//         <ButtonPanel clickHandler={this.handleClick} />
+//       </>
+//     );
+//   }
+// }
+
+const App = () => {
+  const [data, setData] = useState({ total: null, next: null, operation: null });
+
+  const handleClick = buttonName => {
+    setData(calculate(data, buttonName));
+  };
+  return (
+    <>
+      <Display result={data.next || data.total || '0'} />
+      <ButtonPanel clickHandler={handleClick} />
+    </>
+  );
+};
+
+export default App;
